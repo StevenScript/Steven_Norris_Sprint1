da5f1e3a17e6e046355d2e2922267038ef330ac7
Steven Norris <sdn709466@gmail.com>
2024-06-24 02:41:48 -0230

    First night's work

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Steven_Norris_Sprint1
+
 Term 2 Sprint 1 Repository - Project is a website for the fictional fast food chain "Gary Blue's Diner"
+
+--06-24-2024--
+
+**FOCUS:** LANDING PAGE
+
+**DONE**
+
+- Base workspace setup
+- created Navbar
+- Created offer display
+
+**TO-DO**
+
+- Bottom Navbar
+- Add socials and fine print below
---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("JavaScript Loaded");
+});
+
+// scripts.js
+
+$(document).ready(function () {
+  $(".carousel").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: "linear",
+  });
+});
